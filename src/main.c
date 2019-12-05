40cfdbb0a5312b2b3f102bc5fafa13d9559bc540
slouf <slouf@miniLSL.local>
2019-12-05 17:15:39 +0100

    test version

--- src/main.c
+++ src/main.c
@@ -13,12 +13,9 @@
  * 
  */
 
- #if defined (__AVR_ATtiny2313__)
-	#define VERSION      100
-#elif defined (__AVR_ATtiny4313__)
-	#define VERSION      200
-#endif
-#define VERSION_SUB      212
+ 
+#define VERSION      100
+#define VERSION_SUB  1
 
 #define F_CPU         16000000L
 
@@ -56,64 +53,8 @@
 #define TWIADDR2 0b00110000
 uint8_t twiaddr = TWIADDR1;
 
-/* Thermometer */
-#define THERM_ERROR_ERR 0x00
-#define THERM_ERROR_OK  0x01
-#define THERM_TEMP_ERR  0x80
-
-/* Thermometer Connections  */
-#define THERM_PORT 	PORTB
-#define THERM_DDR 	DDRB
-#define THERM_PIN 	PINB
-#define THERM_DQ 	PB4
-#define TEMPERATURE_DELAY    2000
-#define TEMPERATURE_TRESHOLD 28       //fan on
-#define TEMPERATURE_TRESHOLD_STOP 40  //fan off, when led is off
-#define TEMPERATURE_MAX      40  //fan max
-#define FAN_MIN              80  //minimum pwm fan 30%
-#define FAN_MAX              255 //max pwm fan 100%
-#define TEMPERATURE_OVERHEAT 45 
-
-/* Utils */
-#define THERM_INPUT_MODE() 		THERM_DDR&=~(1<<THERM_DQ)
-#define THERM_OUTPUT_MODE()		THERM_DDR|=(1<<THERM_DQ)
-#define THERM_LOW() 			THERM_PORT&=~(1<<THERM_DQ)
-#define THERM_HIGH() 			THERM_PORT|=(1<<THERM_DQ)
-
-#define THERM_CMD_CONVERTTEMP 	0x44
-#define THERM_CMD_RSCRATCHPAD 	0xbe
-#define THERM_CMD_WSCRATCHPAD 	0x4e
-#define THERM_CMD_CPYSCRATCHPAD 0x48
-#define THERM_CMD_RECEEPROM 	0xb8
-#define THERM_CMD_RPWRSUPPLY 	0xb4
-#define THERM_CMD_SEARCHROM 	0xf0
-#define THERM_CMD_READROM 		0x33
-#define THERM_CMD_MATCHROM 		0x55
-#define THERM_CMD_SKIPROM 		0xcc
-#define THERM_CMD_ALARMSEARCH 	0xec
-
-// Scratchpad locations
-#define TEMP_LSB        0
-#define TEMP_MSB        1
-#define HIGH_ALARM_TEMP 2
-#define LOW_ALARM_TEMP  3
-#define CONFIGURATION   4
-#define INTERNAL_BYTE   5
-#define COUNT_REMAIN    6
-#define COUNT_PER_C     7
-#define SCRATCHPAD_CRC  8
-
-	
-//teplomer
-int8_t therm_ok = 0;
-uint8_t scratchpad[9];
-int8_t rawTemperature = 25;
-uint16_t crc = 0xFFFF;
-
 // pwm
 int16_t val, nval = 0;
-
-//	#define PWM_FREQ      480    //test
 
 #define PWM_FREQ      240    
 
@@ -157,23 +98,7 @@ volatile uint8_t bitmask = 0;
  *  	-  TODO: overit dobu trvani preruseni pri I2C komunikaci
  */
 
-#if (PWM_FREQ == 480) //test version
-#define LOOP_COUNT 9
-#define MAX_LOOP   LOOP_COUNT-1
-const uint8_t tbl_loop_bitmask[LOOP_COUNT] = { 8, 9, 8, 11, 10, 11, 10, 11, 9 };
-const uint16_t tbl_loop_len[LOOP_COUNT] = {3069,5117,6141,10237,14333,22525,26621,30717,32765};
-#define WAIT_0 8
-#define WAIT_1 16
-#define WAIT_2 25
-#define WAIT_3 57
-#define WAIT_4 121
-#define WAIT_5 249
-#define WAIT_6a 249
-#define WAIT_6b 249
-#define WAIT_7a 249
-#define WAIT_7b 505
-#define WAIT_7c 249
-#else
+
 #define LOOP_COUNT 9
 #define MAX_LOOP   LOOP_COUNT-1
 const uint8_t tbl_loop_bitmask[LOOP_COUNT] = { 8, 9, 8, 11, 10, 11, 10, 11, 9 };
@@ -189,13 +114,11 @@ const uint16_t tbl_loop_len[LOOP_COUNT] = { 6133, 10229, 12277, 20469, 28661, 45
 #define WAIT_7a  505
 #define WAIT_7b  1017
 #define WAIT_7c  505
-#endif
 
 int16_t incLedValues[PWM_CHANNELS + 1] = { 0 };
-int16_t actLedValues[PWM_CHANNELS] = { 0 };
-
-int16_t *p_actLedValues = actLedValues;
 int16_t *p_incLedValues = incLedValues;
+int16_t ledValues[PWM_CHANNELS + 1] = { 0 };
+int16_t *p_ledValues = ledValues;
 
 uint8_t _data[PWM_BITS] = { 0 };      //double buffer for port values
 uint8_t _data_buff[PWM_BITS] = { 0 };
@@ -208,31 +131,6 @@ volatile uint8_t inc_pwm_data = 1;
 volatile uint8_t effect = 0;
 
 int16_t tmp;
-
-unsigned long milis_time = 0;
-
-#if (VERSION == 200)
-//interpolace s mezemi min a max, 8bit
-static uint8_t map_minmax(uint8_t x, uint8_t in_min, uint8_t in_max,
-		uint8_t out_min, uint8_t out_max) {
-	int16_t ret = (x - in_min) * (out_max - out_min) / (in_max - in_min)
-			+ out_min;
-	return (uint8_t) (ret > out_max ? out_max : ret < out_min ? out_min : ret);
-}
-static int16_t map(int16_t x, int16_t in_min, int16_t in_max, int16_t out_min, int16_t out_max) {
-	int32_t tmp1 = (x - in_min) * (out_max - out_min);
-	int16_t tmp2 = in_max - in_min;
-	return (tmp1 / tmp2) + out_min;
-}
-#endif
-
-
-/*
-//linear interpolation
-static inline long map(long x, long in_min, long in_max, long out_min,
-		long out_max) {
-	return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
-}*/
 
 /*
  * NO-PWM / bit angle modulation
@@ -322,11 +220,14 @@ ISR(TIMER1_COMPB_vect) {
 void pwm_update(void) {
 	//clear
 	memset(_d_b, 0, 12);
+					
 	//rearrange values to ports
 	for (int i = 0; i < PWM_BITS; i++) {
 		for (int j = 0; j < PWM_CHANNELS; j++) {
+			uint8_t r = pgm_read_byte(&sw_resistor[j]);
+			int16_t led_r = (p_ledValues[j] * r)/100;
 			_d_b[(PWM_BITS - 1) - i] = (_d_b[(PWM_BITS - 1) - i] << 1)
-					| (((actLedValues[j]) >> ((PWM_BITS - 1) - i)) & 0x01);
+					| (((led_r) >> ((PWM_BITS - 1) - i)) & 0x01);
 		}
 	}
 
@@ -337,116 +238,6 @@ void pwm_update(void) {
 	//set new data flag
 	newData = 1;
 }
-
-#if (VERSION == 200)
-/*
- * Rutiny pro teplomer
- *
- */
-
-uint8_t ds18b20crc8(uint8_t *data, uint8_t length) {
-	//Generate 8bit CRC for given data (Maxim/Dallas)
-
-	uint8_t i = 0;
-	uint8_t j = 0;
-	uint8_t mix = 0;
-	uint8_t crc = 0;
-	uint8_t byte = 0;
-
-	for (i = 0; i < length; i++) {
-		byte = data[i];
-
-		for (j = 0; j < 8; j++) {
-			mix = (crc ^ byte) & 0x01;
-			crc >>= 1;
-			if (mix)
-				crc ^= 0x8C;
-			byte >>= 1;
-		}
-	}
-	return crc;
-}
-
-static uint8_t therm_reset() {
-
-	uint8_t i;
-	//Pull line low and wait for 480uS
-	//ATOMIC_BLOCK(ATOMIC_FORCEON) {
-	//cekani na casove okno
-	// 480us = cca 7680 cyklu, ktere by mely byt k dispozici
-	while (OCR1B < 45045) {
-		;
-	};
-	//ATOMIC_BLOCK(ATOMIC_FORCEON) {
-	THERM_LOW();
-	THERM_OUTPUT_MODE();
-	_delay_us(480);
-	THERM_INPUT_MODE();
-	_delay_us(40);
-	i = (THERM_PIN & (1 << THERM_DQ));
-	//}
-	_delay_us(480);
-	//Return the value read from the presence pulse (0=OK, 1=WRONG)
-	return i;
-}
-
-static void therm_write_bit(uint8_t bit) {
-	//Pull line low for 1uS
-	while (OCR1B < 6133) {
-		;
-	};
-	THERM_LOW();
-	THERM_OUTPUT_MODE();
-	_delay_us(1);
-	//If we want to write 1, release the line (if not will keep low)
-	if (bit)
-		THERM_INPUT_MODE();
-	//Wait for 60uS and release the line
-	_delay_us(60);
-	THERM_INPUT_MODE();
-}
-
-static uint8_t therm_read_bit(void) {
-
-	uint8_t bit = 0;
-	//Pull line low for 1uS
-	while (OCR1B < 6133) {
-		;
-	};
-	THERM_LOW();
-	THERM_OUTPUT_MODE();
-	_delay_us(1);
-	//Release line and wait for 14uS
-	THERM_INPUT_MODE();
-	_delay_us(10);
-	//Read line value
-	if (THERM_PIN & (1 << THERM_DQ))
-		bit = 1;
-	//Wait for 45uS to end and return read value
-	_delay_us(45);
-	return bit;
-}
-
-static uint8_t therm_read_byte(void) {
-	uint8_t i = 8, n = 0;
-	while (i--) {
-		n >>= 1; //Shift one position right and store read value
-		n |= (therm_read_bit() << 7);
-	}
-	return n;
-}
-
-static void therm_write_byte(uint8_t byte) {
-
-	uint8_t i = 8;
-	while (i--) {
-		//Write actual bit and shift one position right to make the next bit ready
-		therm_write_bit(byte & 1);
-		byte >>= 1;
-	}
-}
-
-#endif
 
 
 /*
@@ -472,16 +263,16 @@ uint8_t i2cReadFromRegister(uint8_t reg) {
 
 	switch (reg) {
 	case reg_LED_L_0 ... reg_LED_H_6:
-		ret = (*((uint8_t *) (p_actLedValues) + reg));		
+		ret = (*((uint8_t *) (p_ledValues) + reg));		
 		break;
 	case reg_MASTER:
 		ret = pwm_status;
 		break;
 	case reg_THERM_STATUS:  //temperature status
-		ret = therm_ok;
+		ret = 0;
 		break;
 	case reg_RAW_THERM:
-		ret = rawTemperature;
+		ret = 0;
 		break;
 	case reg_VERSION_MAIN:
 		ret = VERSION;
@@ -492,51 +283,6 @@ uint8_t i2cReadFromRegister(uint8_t reg) {
 	}
 	
 	return ret;
-}
-
-
-/*
- * Milis()
- */
-
-#define PRESCALER  8
-#define clockCyclesToMicroseconds(a) ( ((a) * 1000L) / (F_CPU / 1000L) )
-#define MICROSECONDS_PER_TIMER0_OVERFLOW (clockCyclesToMicroseconds(PRESCALER * 256))
-#define MILLIS_INC (MICROSECONDS_PER_TIMER0_OVERFLOW / 1000)
-// the fractional number of milliseconds per timer0 overflow. we shift right
-// by three to fit these numbers into a byte. (for the clock speeds we care
-// about - 8 and 16 MHz - this doesn't lose precision.)
-#define FRACT_INC ((MICROSECONDS_PER_TIMER0_OVERFLOW % 1000) >> 3)
-#define FRACT_MAX (1000 >> 3)
-
-volatile unsigned long timer0_overflow_count = 0;
-volatile unsigned long timer0_millis = 0;
-static unsigned char timer0_fract = 0;
-
-ISR(TIMER0_OVF_vect) {
-	unsigned long m = timer0_millis;
-	unsigned char f = timer0_fract;
-
-	m += MILLIS_INC;
-	f += FRACT_INC;
-	if (f >= FRACT_MAX) {
-		f -= FRACT_MAX;
-		m += 1;
-	}
-
-	timer0_fract = f;
-	timer0_millis = m;
-	timer0_overflow_count++;
-}
-
-unsigned long millis() {
-	unsigned long m;
-	uint8_t oldSREG = SREG;
-
-	cli();
-	m = timer0_millis;
-	SREG = oldSREG;
-	return m;
 }
 
 static void set_fan(uint8_t pwm) {
@@ -568,7 +314,7 @@ static uint16_t crc16_update(uint16_t crc, uint8_t a) {
 static uint8_t checkActLedVal() {
 	uint8_t lv = 0;
 	for (uint8_t i = 0; i < PWM_CHANNELS; i++) {
-		if (actLedValues[i] > 0) {
+		if (p_ledValues[i] > 0) {
 			lv = 1;
 			break;
 		}
@@ -586,22 +332,7 @@ int main(void) {
 	dbg_tx_init();
 #endif
 
-// rizeni chodu
-unsigned long tempTicks = 0;
-unsigned long i_timeTicks = 0;
-uint8_t overheat = 0;
-int8_t istep = 0;
-
-
-//priznak
-uint8_t updateStart = 0; 
-
 //bufer
-int16_t ledValues[PWM_CHANNELS + 1] = { 0 };
-int16_t prevLedValues[PWM_CHANNELS + 1] = { 0 };
-int16_t *p_ledValues = ledValues;
-int16_t *p_prevLedValues = prevLedValues;
-
 	_d = _data; 
 	_d_b = _data_buff;
 
@@ -653,11 +384,8 @@ if (!(PINB & (1 << PB6))) {
 	TIFR |= (1 << TOV0);
 	TIMSK |= (1 << TOIE0);
 
-#if (PRESCALER == 64L)
-	TCCR0B |= (1 << CS01) | (1 << CS00); // prescale 64
-#else
 	TCCR0B |= (1 << CS01); // prescale 8
-#endif
+
 	//fast PWM
 	TCCR0A |= (1 << COM0A1) | (1 << WGM00) | (1 << WGM01);
 
@@ -673,34 +401,7 @@ if (!(PINB & (1 << PB6))) {
 
 	sei();
 
-#if (VERSION == 200)
-	set_fan(255);
-	_delay_ms(2000);
-	set_fan(0);
-	/*
-	 * Inicializace teplomeru
-	 * start konverze
-	 */
-	if (therm_reset()) {
-		therm_ok = 0;
-	} else {
-		therm_ok = 1;
-		therm_reset();
-		therm_write_byte(THERM_CMD_SKIPROM);
-		therm_write_byte(THERM_CMD_WSCRATCHPAD);
-		therm_write_byte(0); //Th register
-		therm_write_byte(0); //TL register
-		therm_write_byte(0b00011111); //conf register, 9bit resolution
-
-		therm_reset();
-		therm_write_byte(THERM_CMD_SKIPROM);
-		therm_write_byte(THERM_CMD_CONVERTTEMP);
-		while (!therm_read_bit())
-			;
-	}
-#endif
-
-#define MASTER_TIMEOUT  2 //sec
+#define MASTER_TIMEOUT  10 //sec
 	//cekej  na pwm_status from master
 	uint8_t wait_tmp = 0;
 
@@ -720,81 +421,15 @@ if (!(PINB & (1 << PB6))) {
 	while (1) {
 		wdt_reset();
 
-#if (VERSION == 200)
-		/*
-		 * Mereni teploty
-		 */
-		if (therm_ok && ((millis() - tempTicks) >= TEMPERATURE_DELAY)) { 
-			//precteni teploty a start nove konverze
-			therm_reset();
-			therm_write_byte(THERM_CMD_SKIPROM);
-			therm_write_byte(THERM_CMD_RSCRATCHPAD);
-
-			uint8_t i = 0;
-			do {
-				scratchpad[i] = therm_read_byte();
-			} while (++i < 9);
-
-			if (ds18b20crc8(scratchpad, 8) == scratchpad[8]) {
-				rawTemperature = scratchpad[0] >> 4;
-				rawTemperature |= (scratchpad[1] & 0x7) << 4;
-			}
-
-			/*
-			 * ventilator dle teploty, pokud je ledval > 0
-			 */
-			if ((checkActLedVal() == 0)
-					&& (rawTemperature < TEMPERATURE_TRESHOLD_STOP)) {
-				set_fan(0);
-			} else {
-				if (rawTemperature != THERM_TEMP_ERR) {
-					if (rawTemperature < TEMPERATURE_TRESHOLD) {
-						set_fan(0);
-					} else {
-						uint8_t fanVal = map_minmax(rawTemperature,
-								TEMPERATURE_TRESHOLD, TEMPERATURE_MAX, FAN_MIN,
-								FAN_MAX);
-						if (fanVal < 150)
-							set_fan(FAN_MAX);
-						_delay_ms(500);
-						set_fan(fanVal);
-					}
-					overheat = rawTemperature > TEMPERATURE_OVERHEAT?1:0;
-				}
-			}
-			/*
-			 * start dalsiho mereni
-			 */
-			therm_reset();
-			therm_write_byte(THERM_CMD_SKIPROM);
-			therm_write_byte(THERM_CMD_CONVERTTEMP);
-			tempTicks = millis();
-		} 
-		
-		if (!therm_ok) {
-			//teplomer nefunguje, ale chladit potrebujeme
-			if (checkActLedVal() == 0) {
-				set_fan(0);
-			} else {
-				set_fan(FAN_MAX);
-			}
+		//fan
+		if (checkActLedVal() == 0) {
+			set_fan(0);
+		} else {
+			set_fan(255);
 		}
 
-#else //tiny 2313
-	if (checkActLedVal() == 0) {
-				set_fan(0);
-			} else {
-				set_fan(FAN_MAX);
-			}
-#endif 
-
-
-		/*
-		 *  Hlavni rizeni
-		 */
+		 // Hlavni rizeni
 		uint16_t xcrc = 0xffff;
-
-		milis_time = millis();
 
 		if (pwm_status == MASTER) {
 			if (inc_pwm_data == 0) {  //dostali jsme data, kontrola CRC
@@ -804,12 +439,10 @@ if (!(PINB & (1 << PB6))) {
 				}
 
 				if (xcrc == 0) {
-					int16_t *tmpptr = p_prevLedValues;
-					p_prevLedValues = p_ledValues;
-					p_ledValues = p_incLedValues;
-					p_incLedValues = tmpptr;
-					//priznak startu interpolace
-					updateStart=1;
+					int16_t *tmpptr = p_incLedValues;
+					p_incLedValues = p_ledValues;
+					p_ledValues = tmpptr;
+					pwm_update();
 				}
 				inc_pwm_data = 1;
 			}		
@@ -817,32 +450,12 @@ if (!(PINB & (1 << PB6))) {
 			//test. provoz
 			//zapne kazdou led na testovaci hodnotu			
 			for (uint8_t i = 0; i < PWM_CHANNELS; i++) {
-				actLedValues[i] = 1000;				
+				p_ledValues[i] = 1000;	
+
 			}
 			//updateStart = 1;			
 			pwm_update();
-		}
-
-		if (updateStart == 1) {
-#define ISTEPS       100 //pocet kroku
-#define ISTEPTIMEOUT 10  //ms mezi kroky, celkovy cas prechodu ms = ISTEPS * ISTEPTIMEOUT
-			if ((milis_time - i_timeTicks) > ISTEPTIMEOUT) {
-				i_timeTicks = milis_time;
-				for (uint8_t x = 0; x < PWM_CHANNELS; x++) {
-					int16_t tmp = istep * (p_ledValues[x] - p_prevLedValues[x]);
-					actLedValues[x] =  p_prevLedValues[x] + (  tmp / ISTEPS);	
-					//softwarove omezeni proudu
-					uint8_t res = pgm_read_byte(&sw_resistor[x]);
-					actLedValues[x] = (actLedValues[x] * res)/100;
-					//omezeni pri prehrati ?? :TODO
-					//actLedValues[x] = overheat?actLedValues[x] / 2:actLedValues[x];			
-				}				
-				if (++istep > ISTEPS) {
-					updateStart = 0;
-					istep = 0;
-				}
-				pwm_update();				
-			}			
+			_delay_ms(1000);
 		}								
 	}
 }
